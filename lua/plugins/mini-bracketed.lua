411e3d3b0e5b22806eb9fec2450231b9340f5c4e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-01-28 20:46:05 +0700

    chore(mini-bracketed): disable treesitter move

--- lua/plugins/mini-bracketed.lua
+++ lua/plugins/mini-bracketed.lua
@@ -1,4 +1,5 @@
 ---@type NvPluginSpec
+-- NOTE: Neovim Lua plugin to go forward/backward with square brackets. Part of 'mini.nvim' library.
 return {
   "echasnovski/mini.bracketed",
   event = "VeryLazy",
