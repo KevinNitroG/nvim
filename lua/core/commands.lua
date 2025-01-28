b035f54f90ab17ab9d1f86f911a4d6c6d2a58144
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-01-28 21:28:26 +0700

    feat(commands): add "ClearRegister"

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -86,3 +86,36 @@ end, { desc = "Join empty lines", bang = true, nargs = "?" })
 command("NvChadMasonInstallAll", function()
   require("nvchad.mason").install_all()
 end, { desc = "NvChad Mason Install All" })
+
+command("ClearRegister", function(args)
+  if #args.fargs == 0 then
+    local registers = {
+      '"',
+      "-",
+      "/",
+      "*",
+      "+",
+      "=",
+      "_",
+      unpack(vim.fn.range(0, 9)), -- Registers 0-9
+      unpack(vim.fn.map(vim.fn.range(97, 122), function(_, v)
+        return string.char(v)
+      end)), -- a-z
+    }
+    -- Clear each register
+    for _, reg in ipairs(registers) do
+      vim.fn.setreg(reg, "")
+    end
+
+    vim.notify "All registers have been cleared"
+    return
+  end
+  for _, reg in ipairs(args.fargs) do
+    if vim.fn.getreg(reg) ~= nil then
+      vim.fn.setreg(reg, "")
+      print("Cleared register: " .. reg)
+    else
+      print("Invalid register: " .. reg)
+    end
+  end
+end, { desc = "Clear register data", nargs = "*" })
