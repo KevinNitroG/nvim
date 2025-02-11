15a692d59648ad04fc52a4c174de5bfc69498beb
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-02-11 21:23:17 +0700

    fix(chezmoi-nvim): ignore some chezmoi file to apply watch

--- lua/plugins/chezmoi-nvim.lua
+++ lua/plugins/chezmoi-nvim.lua
@@ -5,10 +5,22 @@ return {
   "xvzc/chezmoi.nvim",
   enabled = vim.fn.executable "chezmoi" == 1,
   init = function()
+    local chezmoi_dir = os.getenv "HOME" .. "/.local/share/chezmoi/"
+    local exclude_patterns = {
+      chezmoi_dir .. "/?(.)chezmoi*",
+      chezmoi_dir .. "/?(.)chezmoi*/",
+    }
+
     vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-      pattern = { os.getenv "HOME" .. "/.local/share/chezmoi/*" },
-      callback = function(ev)
-        local bufnr = ev.buf
+      pattern = { chezmoi_dir .. "*" },
+      callback = function(args)
+        local bufnr = args.buf
+        local file_path = vim.fn.expand("%:p", bufnr)
+        for _, pattern in ipairs(exclude_patterns) do
+          if vim.fn.globpath(file_path, pattern) then
+            return
+          end
+        end
         local edit_watch = function()
           require("chezmoi.commands.__edit").watch(bufnr)
         end
